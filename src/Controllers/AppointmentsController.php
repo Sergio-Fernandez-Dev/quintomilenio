4d9f2407bae188c30abccb618c2d9ed02ac5ce76
silviacoder <silviasgpg@gmail.com>
2022-07-18 15:08:54 +0200

    creating store and  edit

--- src/Controllers/AppointmentsController.php
+++ src/Controllers/AppointmentsController.php
@@ -16,7 +16,21 @@ class AppointmentController
             $this->delete($_GET['id']);
             return;
         }
+
+        if(isset($_GET['action']) == 'store'){
+            $this->store($_POST);
+            return; 
+        }
+
+        if(isset($_GET['action']) == 'edit'){
+            $this->edit($_GET['id']);
+            return;
+
+
+        }
+
         $this->index();
+
     }
 
     public function index()
@@ -30,20 +44,38 @@ class AppointmentController
     {
         new View('form');
     }
-    public function store()
-    {
+
+    public function store(array $request){
         //TO-DO
-    }
-    public function edit()
-    {
-        //TO-DO
+        $model = new Appointment(
+            id: null,
+            $request["name"],
+            $request["phone"],
+            $request["email"],
+            $request["user_query"],
+            date_time: null,
+        );
+
+        $model->saveAppointment();
+        $this->index();
+
     }
 
-    public function delete(int $id) {
+    public function edit(int $id)
+    {
+        //TO-DO
+        $model = new Appointment();
+        $appointment = $model->findById($id);
+
+        new View("form",["appointment" =>$appointment]);
+
+    }
+
+    public function delete(int $id) 
     {
         $model = new Appointment();
         $appointment = $model->findById($id);
-        $appointment->delete();
+        $appointment->destroy();
 
         $this->index();
     }
