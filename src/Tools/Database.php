c6b2ee85aa9cf9c5c7f1075bce9278eb1991ed7a
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-07-19 11:28:55 +0200

    changing db pass

--- src/Tools/Database.php
+++ src/Tools/Database.php
@@ -23,7 +23,7 @@ class Database
     {
         $host = "localhost";
         $user = "root";
-        $pass = "";
+        $pass = "root";
         $database = "consultorioquintomilenio";
         $charset = "utf-8";
         $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
