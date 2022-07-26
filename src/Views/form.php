9b12e8aff2f8731a7792b7a7287075cb830cc8db
Majifer <fjmario99@gmail.com>
2022-07-26 14:46:52 +0200

    Edit Form View

--- src/Views/form.php
+++ src/Views/form.php
@@ -1,7 +1,26 @@
 <html>
 
 <?php
+
+use PhpParser\Node\Name;
+
     require_once("Components/layout.php");
+      
+      $name = "";
+      $phone = "";
+      $email= "";
+      $user_query = "";
+     if($data["action"]=="update"){
+        
+        $appointment = $data["appointments"];
+        $name = $appointment->getName();
+        $phone = $appointment->getPhone();
+        $email = $appointment->getEmail();
+        $userQuery = $appointment->getUserQuery();
+        
+    }
+
+    //echo(var_dump ($data["appointments"]));
 ?>
 
 <body>
@@ -13,18 +32,18 @@
 <main>
     <form method="post" action='?action=<?php echo $data['action'] ?>'>
         <label for="name">Nombre y apellidos  </label>
-            <input type="text" name="name" placeholder="Nombre del cliente" required >
+            <input type="text" name="name" value="<?php echo $name; ?>"  placeholder="Nombre del cliente" required >
        
 
         <label for="email">Email</label>
-            <input type="text" name="email" placeholder="Email del cliente" required >
+            <input type="text" name="email"value="<?php echo $email; ?>" placeholder="Email del cliente" required >
         
 
         <label for="phone">Telefono</label>
-            <input type="text" name="phone" placeholder="Telefono del cliente" required >
+            <input type="text" name="phone"value="<?php echo $phone; ?>" placeholder="Telefono del cliente" required >
        
         <label for="issue">Consulta</label>
-        <textarea name="user_query" rows="10" cols="50" required></textarea>
+        <textarea name="user_query" rows="10" cols="50" required><?php echo $userQuery; ?> </textarea>
         <button type="reset" class="btn btn-outline-danger">Cancelar</button>
         <button type="submit" class="btn btn-success">Guardar</button>
     </form>
