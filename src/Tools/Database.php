cc9bd5e318be398ca466aa7610eaf5171fef8193
Mario Rubio <marioastur@gmail.com>
2022-07-28 14:30:54 +0200

    Form edit and update ok

--- src/Tools/Database.php
+++ src/Tools/Database.php
@@ -22,14 +22,14 @@ class Database
     private function setConnection()
     {
         $host = "localhost";
-        $user = "root";
-        $pass = "root";
+        $user = "consultorio";
+        $pass = "RPO*wJsJfxHsdU/.";
         $database = "consultorioquintomilenio";
         $charset = "utf-8";
         $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
         $pdo = new PDO("mysql:host=$host;dbname=$database;charset$charset", $user, $pass, $options);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
+
         return $pdo;
     }
 
--- src/Views/form.php
+++ src/Views/form.php
@@ -37,37 +37,37 @@ if ($data["action"] == "update") {
                 <p class='card-tittle'>Alta/Modificacion</p>
 
             </div>
-                <div class='card-body '>
+            <div class='card-body '>
 
-                    <form method="post" class="form-edit" action='?action=<?php echo $data['action'] ?>&id=<?php echo $id ?>'>
+                <form method="post" class="form-edit" action='?action=<?php echo $data['action'] ?>&id=<?php echo $id ?>'>
 
-                        <p class='card-data'></p>
-                        <label for="name">Nombre y apellidos </label>
-                        <input type="text" class="form-control" name="name" value="<?php echo $name; ?>" placeholder="Nombre del cliente" required>
-
-
-                        <label for="email">Email</label>
-                        <input type="text" class="form-control" name="email" value="<?php echo $email; ?>" placeholder="Email del cliente" required>
+                    <p class='card-data'></p>
+                    <label for="name">Nombre y apellidos </label>
+                    <input type="text" class="form-control" name="name" value="<?php echo $name; ?>" placeholder="Nombre del cliente" required>
 
 
-                        <label for="phone">Telefono</label>
-                        <input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>" placeholder="Telefono del cliente" required>
+                    <label for="email">Email</label>
+                    <input type="text" class="form-control" name="email" value="<?php echo $email; ?>" placeholder="Email del cliente" required>
 
-                        <label for="issue">Consulta</label>
-                        <textarea name="user_query" class="form-control" rows="10" cols="50" required><?php echo $userQuery; ?> </textarea>
-                       
-                    </form>
+
+                    <label for="phone">Telefono</label>
+                    <input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>" placeholder="Telefono del cliente" required>
+
+                    <label for="issue">Consulta</label>
+                    <textarea name="user_query" class="form-control" rows="10" cols="50" required><?php echo $userQuery; ?> </textarea>
+
+
                     <div class="buttons-container">
                         <div class='buttons'>
                             <a href="/quintomilenio">
-                            <button type="reset"  class="secondary-button">Cancelar</button>
+                                <button type="reset" class="secondary-button">Cancelar</button>
                             </a>
-                        
-                        
                             <button type="submit" class="main-button">Guardar</button>
                         </div>
                     </div>
-                </div>
+                </form>
+
+            </div>
         </div>
 
     </main>
