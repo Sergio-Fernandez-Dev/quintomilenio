3f64cf67b6a68a403f25aea9bcc797201749c32d
silviacoder <silviasgpg@gmail.com>
2022-07-28 17:24:34 +0200

    footer ok

--- src/Views/components/footer/footer.php
+++ src/Views/components/footer/footer.php
@@ -1,4 +1,4 @@
 <div class="footer-container">
     <img src="/quintomilenio/src/assets/img/the-crazy-house-team-logo-transparente.png">
-    <h3>The crazy House Team</h3>
+    <h3>The Crazy House Team</h3>
 </div>
