e3f7542a833295e4bcb91e7d9976a7c5c012c5da
Trabanco <btrabanco@gmail.com>
2022-07-28 09:06:58 +0200

    iconos

--- src/Views/Components/card/card.php
+++ src/Views/Components/card/card.php
@@ -1,14 +1,14 @@
-<div class='card mb-3' style='max-width: 18rem;'>
+<div class='card mb-3'>
     <div class='card-header'>
         <h3><?php echo $name ?></h3>
         <p class='card-data'><?php echo $email ?></p>
         <p class='card-data'><?php echo $phone ?></p>
         <a href=<?php echo '?action=edit&id=' . $id ?>>
-            <button type='button' class='btn btn-primary'>m</button>
+            <button type='button' class='btn btn-light-primary' >🖊️</button>
         </a>
 
         <a href=<?php echo '?action=delete&id=' . $id ?>>
-            <button type='button' class='btn btn-primary'>D</button>
+            <button type='button' class='btn btn-light-danger'>🗑️</button>
         </a>
     </div>
     <div class='card-body '>
