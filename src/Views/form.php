189a496e671169652a515489333c2cded78d484c
Majifer <fjmario99@gmail.com>
2022-07-28 09:06:26 +0200

    Creating Form Style

--- src/Views/form.php
+++ src/Views/form.php
@@ -30,23 +30,39 @@ if ($data["action"] == "update") {
     ?>
 
     <main>
-        <form method="post" action='?action=<?php echo $data['action'] ?>&id=<?php echo $id ?>'>
-            <label for="name">Nombre y apellidos </label>
-            <input type="text" name="name" value="<?php echo $name; ?>" placeholder="Nombre del cliente" required>
+        <link rel="stylesheet" href="src/Views/form.css">
+        <div class='card mb-3' style='max-width: 80em;' style='max-height: 140em;'>
+            <div class='card-header'>
+                <p class='card-tittle'>Alta/Modificacion</p>
+                <div class='card-body '>
+
+                    <form method="post" action='?action=<?php echo $data['action'] ?>&id=<?php echo $id ?>'>
+
+                        <p class='card-data'></p>
+                        <label for="name">Nombre y apellidos </label>
+                        <input type="text" name="name" value="<?php echo $name; ?>" placeholder="Nombre del cliente" required>
 
 
-            <label for="email">Email</label>
-            <input type="text" name="email" value="<?php echo $email; ?>" placeholder="Email del cliente" required>
+                        <label for="email">Email</label>
+                        <input type="text" name="email" value="<?php echo $email; ?>" placeholder="Email del cliente" required>
 
 
-            <label for="phone">Telefono</label>
-            <input type="text" name="phone" value="<?php echo $phone; ?>" placeholder="Telefono del cliente" required>
+                        <label for="phone">Telefono</label>
+                        <input type="text" name="phone" value="<?php echo $phone; ?>" placeholder="Telefono del cliente" required>
 
-            <label for="issue">Consulta</label>
-            <textarea name="user_query" rows="10" cols="50" required><?php echo $userQuery; ?> </textarea>
-            <button type="reset" class="btn btn-outline-danger">Cancelar</button>
-            <button type="submit" class="btn btn-success">Guardar</button>
-        </form>
+                        <label for="issue">Consulta</label>
+                        <textarea name="user_query" rows="10" cols="50" required><?php echo $userQuery; ?> </textarea>
+                       
+                    </form>
+                    <div class='cancellButton'>
+                        <button type="reset" class="btn btn-outline-danger">Cancelar</button>
+                    </div>
+                    <div class="saveButton">
+                        <button type="submit" class="btn btn-success">Guardar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </main>
 
