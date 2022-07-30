115b87ee61f4be7f7c525036447bb316d20b1417
Mario Rubio <marioastur@gmail.com>
2022-07-30 13:52:32 +0200

    Update Database SQL

--- database.sql
+++ database.sql
@@ -1,57 +1,7 @@
--- phpMyAdmin SQL Dump
+CREATE DATABASE IF NOT EXISTS `consultorioquintomilenio`;
+USE `consultorioquintomilenio`;
 
--- version 5.2.0
-
--- https://www.phpmyadmin.net/
-
---
-
--- Servidor: 127.0.0.1
-
--- Tiempo de generación: 18-07-2022 a las 12:02:56
-
--- Versión del servidor: 10.4.24-MariaDB
-
--- Versión de PHP: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-
-START TRANSACTION;
-
-SET time_zone = "+00:00";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
-
-;
-
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
-
-;
-
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
-
-;
-
-/*!40101 SET NAMES utf8mb4 */
-
-;
-
---
-
--- Base de datos: `consultorioquintomilenio`
-
---
-
--- --------------------------------------------------------
-
---
-
--- Estructura de tabla para la tabla `quintomilenio`
-
---
-
-CREATE TABLE
-    `quintomilenio` (
+CREATE TABLE `quintomilenio` (
         `id` int(11) NOT NULL,
         `name` varchar(40) NOT NULL,
         `phone` varchar(20) NOT NULL,
@@ -60,45 +10,5 @@ CREATE TABLE
         `user_query` text NOT NULL
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
---
-
--- Índices para tablas volcadas
-
---
-
---
-
--- Indices de la tabla `quintomilenio`
-
---
-
 ALTER TABLE `quintomilenio` ADD PRIMARY KEY (`id`);
-
---
-
--- AUTO_INCREMENT de las tablas volcadas
-
---
-
---
-
--- AUTO_INCREMENT de la tabla `quintomilenio`
-
---
-
-ALTER TABLE
-    `quintomilenio` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-
-;
-
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-
-;
-
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
-
-;
+ALTER TABLE `quintomilenio` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
